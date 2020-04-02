44f7521249c7ea61b7027969804dfb09668fdc6e
Frédéric Pinaud <fpinaud17@gmail.com>
2020-04-02 16:17:46 +0200

    Added GetBuckets to get all buckets with limit, name, offset, org, orgID parameters.

--- buckets.go
+++ buckets.go
@@ -2,8 +2,10 @@ package influxdb
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func (c *Client) GetBucketsInSource(id string) (*BucketSource, error) {
@@ -19,6 +21,38 @@ func (c *Client) GetBucketsInSource(id string) (*BucketSource, error) {
 	resp, err := c.httpClient.Do(req)
 
 	defer resp.Body.Close()
+	bucketSource := &BucketSource{}
+	if err := json.NewDecoder(resp.Body).Decode(bucketSource); err != nil {
+		return nil, err
+	}
+
+	return bucketSource, nil
+}
+
+func (c *Client) GetBuckets(limit int, name string, offset int, org string, orgID string) (*BucketSource, error) {
+	if limit == 0 || limit > 100 {
+		return nil, errors.New("limit needs to be between [ 1 ... 100 ]")
+	}
+	if offset < 0 {
+		return nil, errors.New("offset needs to be granter or equal to 0")
+	}
+	log.Printf("[DEBUG] Get buckets")
+
+	params := "/buckets?limit=" + strconv.Itoa(limit) + "&name=" + name + "&offset=" + strconv.Itoa(offset) + "&org=" + org + "&orgID=" + orgID + ""
+	req, err := http.NewRequest(http.MethodGet, c.url.String()+params, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Authorization", c.authorization)
+	resp, err := c.httpClient.Do(req)
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(resp.Status)
+	}
+
 	bucketSource := &BucketSource{}
 	if err := json.NewDecoder(resp.Body).Decode(bucketSource); err != nil {
 		return nil, err
