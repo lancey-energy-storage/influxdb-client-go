bbfbd7fa6deefd02b811b3b8bba20efc121502e8
Frédéric Pinaud <fpinaud17@gmail.com>
2020-04-06 14:41:58 +0200

    Added CreateUser to create a user on the influxdbv2 instance and refactored code for the object UserList

--- users.go
+++ users.go
@@ -1,6 +1,7 @@
 package influxdb
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"log"
@@ -8,7 +9,7 @@ import (
 )
 
 func (c *Client) GetAllUsers() (*UserList, error) {
-	log.Printf("[DEBUG] Get all authorizations")
+	log.Printf("[DEBUG] Get all users")
 
 	req, err := http.NewRequest(http.MethodGet, c.url.String()+"/users", nil)
 	if err != nil {
@@ -32,18 +33,63 @@ func (c *Client) GetAllUsers() (*UserList, error) {
 	return userList, nil
 }
 
+func (c *Client) CreateUser(name string, oauthID string, status string) (*User, error) {
+	if name == "" {
+		return nil, errors.New("a name is requried")
+	}
+
+	log.Printf("[DEBUG] Creation of a user with name %s", name)
+
+	inputData, err := json.Marshal(NewUser{
+		Name:    name,
+		OauthID: oauthID,
+		Status:  status,
+	})
+	req, err := http.NewRequest(http.MethodPost, c.url.String()+"/users", bytes.NewBuffer(inputData))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Authorization", c.authorization)
+	resp, err := c.httpClient.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 201 {
+		return nil, errors.New(resp.Status)
+	}
+
+	user := &User{}
+	if err := json.NewDecoder(resp.Body).Decode(user); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
 type UserList struct {
 	Links struct {
 		Self string `json:"self"`
 	} `json:"links"`
-	Users []struct {
-		Id      string `json:"id"`
-		OauthID string `json:"oauthID"`
-		Name    string `json:"name"`
-		Status  string `json:"status"`
-		Links   struct {
-			Self string `json:"self"`
-			Logs string `json:"logs"`
-		} `json:"links"`
-	} `json:"users"`
+	Users []User `json:"users"`
+}
+
+type User struct {
+	Id      string `json:"id"`
+	OauthID string `json:"oauthID"`
+	Name    string `json:"name"`
+	Status  string `json:"status"`
+	Links   struct {
+		Self string `json:"self"`
+		Logs string `json:"logs"`
+	} `json:"links"`
+}
+
+type NewUser struct {
+	Name    string `json:"name"`
+	OauthID string `json:"oauthID"`
+	Status  string `json:"status"`
 }
